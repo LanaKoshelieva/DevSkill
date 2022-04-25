66b0522af0f5d5da348fbd29a2fd94d4c5469318
Lana Koshelieva <lana.koshelieva@gmail.com>
2022-04-25 20:37:39 +0200

    creato il programa di convensione (ho avuto un weeken molto divertente. grazie ;| )

---
+++ Converter/src/app/home/home.page.ts
@@ -0,0 +1,90 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  title:string = "Conversion of measurements";
+  celsius:number;
+  fahrenheit:number;
+  kilometers:number;
+  miles:number;
+
+  celsiusToFahrenheit (gradiC:number):number {
+    return gradiC * 9 / 5 + 32;
+  }
+
+  fahrenheitToCelsius (gradiF:number):number {
+    return (gradiF-32) * 5 / 9;  
+  }
+
+  milesToKilometers (distanzaM:number):number {
+    return distanzaM * 1.60934;
+  }
+
+  kilometersToMiles (distanzaKm:number):number {
+    return distanzaKm / 1.60934;
+  }
+
+  reset() {
+    console.log("reset");
+    let conferma:boolean = confirm ("Do you really want to reset the data?");
+
+    if (conferma == true) {
+      this.celsius = null;
+      this.fahrenheit = null;
+      this.kilometers = null;
+      this.miles = null;
+    }    
+  }
+
+  cInput(value:number) {
+    if(value == null)
+    {
+      this.fahrenheit = null;
+    }
+    else
+    {
+      this.fahrenheit = this.celsiusToFahrenheit(value);
+    }
+  }
+
+  fInput(value:number) {
+    if(value == null)
+    {
+      this.celsius = null;
+    }
+    else
+    {
+      this.celsius = this.fahrenheitToCelsius(value);
+    }
+  }
+
+  kInput(value:number) {
+    if(value == null)
+    {
+      this.miles = null;
+    }
+    else
+    {
+      this.miles = this.kilometersToMiles(value);
+    }
+  }
+
+  mInput(value:number) {
+    if(value == null)
+    {
+      this.kilometers = null;
+    }
+    else
+    {
+      this.kilometers = this.milesToKilometers(value);
+    }
+  }
+
+}
+
+
